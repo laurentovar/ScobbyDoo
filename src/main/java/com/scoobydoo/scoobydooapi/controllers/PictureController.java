a8bdecd8b3b10928051ac9c43b6f282e69c8c8a9
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-29 18:23:09 -0600

    added the crossorigin in the picture controller

--- src/main/java/com/scoobydoo/scoobydooapi/controllers/PictureController.java
+++ src/main/java/com/scoobydoo/scoobydooapi/controllers/PictureController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(PictureController.BASE_URL)
 public class PictureController {
